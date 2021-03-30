7eb833a0969d131dc869dc7817152a6a44b99d05
Mark Yoo <yjinhyuk@gmail.com>
2021-03-30 21:39:43 +0900

    Add: Pages/ Detail: Login Page is on progress

---
+++ front/pages/SignUp/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div id="container">
+      <Header>Slack in ReactTS</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="cniname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUp;
