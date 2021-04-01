caae1ee1a699e1f1d26294f6124b01e4ea7fae64
Mark Yoo <yjinhyuk@gmail.com>
2021-04-01 23:13:33 +0900

    Update: SignUp/index.tsx, /styles.tsx Detail: signup page's layout settled

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,43 +1,38 @@
 import React from 'react';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
   return (
     <div id="container">
       <Header>Slack in ReactTS</Header>
-      <Form onSubmit={onSubmit}>
+      <Form>
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+            <Input type="email" id="email" name="email" />
           </div>
         </Label>
         <Label id="nickname-label">
           <span>닉네임</span>
           <div>
-            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+            <Input type="text" id="nickname" name="nickname" />
           </div>
         </Label>
         <Label id="password-label">
           <span>패스워드</span>
           <div>
-            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+            <Input type="password" id="password" name="password" />
           </div>
         </Label>
         <Label id="password-check-label">
           <span>비밀번호 확인</span>
           <div>
-            <Input
-              type="password"
-              id="password-check"
-              name="password-check"
-              value={passwordCheck}
-              onChange={onChangePasswordCheck}
-            />
+            <Input type="password" id="password-check" name="password-check" />
           </div>
-          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요</Error>}
           {signUpError && <Error>이미 가입된 이메일입니다.</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다!</Success>}
+          {signUpSuccess && <Success>회원가입되었습니다!</Success>} */}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
