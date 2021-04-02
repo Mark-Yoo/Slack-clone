40c4409027d25ac8fb72722be32451dffa32776c
Mark Yoo <yjinhyuk@gmail.com>
2021-04-02 21:44:59 +0900

    Update: signUp init form settled

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,38 +1,58 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
+  const [email, setEmail] = useState();
+  const [nickname, setNickname] = useState();
+  const [password, setPassword] = useState();
+  const [passwordCheck, setPasswordCheck] = useState();
+  const [mismatchError, setMismatchError] = useState();
+  const [signUpError, setSignUpError] = useState();
+  const [signUpSuccess, setSignUpSuccess] = useState();
+
+  const onSubmit = useCallback(() => {}, []);
+  const onChangeEmail = useCallback(() => {}, []);
+  const onChangeNickname = useCallback(() => {}, []);
+  const onChangePassword = useCallback(() => {}, []);
+  const onChangePasswordCheck = useCallback(() => {}, []);
+
   return (
     <div id="container">
       <Header>Slack in ReactTS</Header>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email" />
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
           </div>
         </Label>
         <Label id="nickname-label">
           <span>닉네임</span>
           <div>
-            <Input type="text" id="nickname" name="nickname" />
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
           </div>
         </Label>
         <Label id="password-label">
           <span>패스워드</span>
           <div>
-            <Input type="password" id="password" name="password" />
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
           </div>
         </Label>
         <Label id="password-check-label">
           <span>비밀번호 확인</span>
           <div>
-            <Input type="password" id="password-check" name="password-check" />
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
           </div>
-          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요</Error>}
           {signUpError && <Error>이미 가입된 이메일입니다.</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다!</Success>} */}
+          {signUpSuccess && <Success>회원가입되었습니다!</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
