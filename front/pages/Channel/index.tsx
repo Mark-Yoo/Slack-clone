04948c5e88ae63d39fba26db9d51b95a412a49ef
Mark Yoo <yjinhyuk@gmail.com>
2021-04-08 04:09:05 +0900

    Add: pages/Channel/index.tsx added

---
+++ front/pages/Channel/index.tsx
@@ -0,0 +1,12 @@
+import React, { VFC } from 'react';
+import Workspace from '@layouts/Workspace';
+
+const Channel: VFC = () => {
+  return (
+    <Workspace>
+      <div>로그인되었습니다!</div>
+    </Workspace>
+  );
+};
+
+export default Channel;
