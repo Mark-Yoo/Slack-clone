df71ee5cbccc4d9a4efd4b773a79f2d5f1646b88
Mark Yoo <yjinhyuk@gmail.com>
2021-04-08 04:11:07 +0900

    Update: route for channel added

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -3,12 +3,14 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 // page 단위의 code splitting을 위한 loadable
 import loadable from '@loadable/component';
+import Workspace from './Workspace';
 // import LogIn from '@pages/LogIn';
 // import SignUp from '@pages/SignUp';
 
 // loadable에 콜백으로 전달
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
       <Redirect exact path="/" to="login" />
       <Route path="/login" component={LogIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/workspace/channel" component={Channel} />
     </Switch>
   );
 };
