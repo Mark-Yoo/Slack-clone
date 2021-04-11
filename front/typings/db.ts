ef67292077977b0b0b4c02de798b6794660e03c3
Mark Yoo <yjinhyuk@gmail.com>
2021-04-11 20:19:01 +0900

    Add: db.ts addded Detail: db types added

---
+++ front/typings/db.ts
@@ -0,0 +1,44 @@
+export interface IUser {
+  id: number;
+  nickname: string;
+  email: string;
+  Workspaces: IWorkspace[];
+}
+
+export interface IUserWithOnline extends IUser {
+  online: boolean;
+}
+
+export interface IChannel {
+  id: number;
+  name: string;
+  private: boolean;
+  WorkspaceId: number;
+}
+
+export interface IChat {
+  id: number;
+  UserId: number;
+  User: IUser;
+  content: string;
+  createdAt: Date;
+  ChannelId: number;
+  Channel: IChannel;
+}
+
+export interface IDM {
+  // DM 채팅
+  id: number;
+  SenderId: number;
+  Sender: IUser;
+  ReceiverId: number;
+  Receiver: IUser;
+  content: string;
+  createdAt: Date;
+}
+
+export interface IWorkspace {
+  id: number;
+  name: string;
+  OwnerId: number;
+}
