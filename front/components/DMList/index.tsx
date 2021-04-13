4f5e71c176b59faf106ccd18a59d9e8b748e3dc5
Mark Yoo <yjinhyuk@gmail.com>
2021-04-14 02:00:42 +0900

    Add: components/DMList Detail: can show whom you can send DM to Detail: can route to each user as difference address to send DM

---
+++ front/components/DMList/index.tsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useState, useEffect, FC } from 'react';
+import useSWR from 'swr';
+import { useParams } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { CollapseButton } from '@components/DMList/styles';
+import fetcher from '@utils/fetcher';
+import { IDM, IUser, IUserWithOnline } from '@typings/db';
+
+interface Props {
+  userData?: IUser;
+}
+
+const DMList: FC<Props> = ({ userData }) => {
+  const [channelCollapse, setChannelCollapse] = useState(false);
+  const [countList, setCountList] = useState<{ [key: string]: number }>({});
+  const [onlineList, setOnlineList] = useState<number[]>([]);
+
+  const { workspace } = useParams<{ workspace?: string }>();
+  const { data: memberData } = useSWR<IUserWithOnline[]>(
+    userData ? `/api/workspaces/${workspace}/members` : null,
+    fetcher,
+  );
+
+  const toggleChannelCollapse = useCallback(() => {
+    setChannelCollapse((prev) => !prev);
+  }, []);
+
+  const resetCount = useCallback((id) => {
+    setCountList((list) => {
+      return {
+        ...list,
+        [id]: 0,
+      };
+    });
+  }, []);
+
+  const onMessage = (data: IDM) => {
+    console.log('DM 도착', data);
+    setCountList((list) => {
+      return {
+        ...list,
+        [data.SenderId]: list[data.SenderId] ? list[data.SenderId] + 1 : 1,
+      };
+    });
+  };
+
+  useEffect(() => {
+    console.log('DMList: workspace is changed', workspace);
+    setOnlineList([]);
+    setCountList({});
+  }, [workspace]);
+
+  return (
+    <>
+      <h2>
+        <CollapseButton collapse={channelCollapse} onClick={toggleChannelCollapse}>
+          <i
+            className="c-icon p-channel_sidebar__section_heading_expand p-channel_sidebar__section_heading_expand--show_more_feature c-icon--caret-right c-icon--inherit c-icon--inline"
+            data-qa="channel-section-collapse"
+            aria-hidden="true"
+          />
+        </CollapseButton>
+        <span>Direct Messages</span>
+      </h2>
+      <div>
+        {!channelCollapse &&
+          memberData?.map((member) => {
+            const isOnline = onlineList.includes(member.id);
+            return (
+              <NavLink key={member.id} activeClassName="selected" to={`/workspace/${workspace}/dm/${member.id}`}>
+                <i
+                  className={`c-icon p-channel_sidebar__presence_icon p-channel_sidebar__presence_icon--dim_enabled c-presence ${
+                    isOnline ? 'c-presence--active c-icon--presence-online' : 'c-icon--presence-offline'
+                  }`}
+                  aria-hidden="true"
+                  data-qa="presence_indicator"
+                  data-qa-presence-self="false"
+                  data-qa-presence-active="false"
+                  data-qa-presence-dnd="false"
+                />
+                <span>{member.nickname}</span>
+                {member.id === userData?.id && <span> (나)</span>}
+              </NavLink>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default DMList;
