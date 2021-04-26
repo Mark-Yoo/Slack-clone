7aa6b17ee5f1a1fd10898fd1eedb95a839860669
Mark Yoo <yjinhyuk@gmail.com>
2021-04-26 21:21:25 +0900

    Update: pages/LogIn

--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,13 +1,14 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, FC } from 'react';
 import axios from 'axios';
 import useSWR from 'swr';
 import { Link, Redirect } from 'react-router-dom';
 import useInput from '@hooks/useInput';
 import fetcher from '@utils/fetcher';
 import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
+import { validate } from 'webpack';
 
-const LogIn = () => {
-  const { data, error, revalidate } = useSWR('http://localhost:3095/api/users', fetcher);
+const LogIn: FC = () => {
+  const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher);
   const [logInError, setLogInError] = useState(false);
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
@@ -16,8 +17,10 @@ const LogIn = () => {
       e.preventDefault();
       setLogInError(false);
       axios
-        .post('http://localhost:3095/api/users/login', { email, password }, { withCredentials: true })
-        .then(() => {
+        .post('/api/users/login', { email, password }, { withCredentials: true })
+        .then((response) => {
+          // mutate(response.data, false);
+          // validate를 true로 설정하게 된다면 optimistic UI는 보통 요청을 보내는 순간 요청이 성공할 거라고 확신할 때에 상태를 먼저 바꾸고 요청을 나중에 보내는 방식이다. 이 때에 나중에 검사를 다시 거치게 된다.
           revalidate();
         })
         .catch((error) => {
@@ -31,7 +34,7 @@ const LogIn = () => {
     return <div>로딩중...</div>;
   }
   if (data) {
-    return <Redirect to="workspace/channel" />;
+    return <Redirect to="workspace/sleact/channel/일반" />;
   }
 
   return (
