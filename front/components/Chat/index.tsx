6da85e527576afb79393daa27de736e16d0f190f
Mark Yoo <yjinhyuk@gmail.com>
2021-04-30 23:13:58 +0900

    Update: components/Chat

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,13 +1,29 @@
 import React, { VFC } from 'react';
+import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
+import { ChatWrapper } from '@components/Chat/styles';
 
 interface Props {
-  key: string;
-  data: IDM[];
+  key: Number;
+  data: IDM;
 }
 
 const Chat: VFC<Props> = ({ key, data }) => {
-  return <div>Chat</div>;
+  const user = data.Sender;
+  return (
+    <ChatWrapper>
+      <div className="chat-img">
+        <img src={gravatar.url(user.email, { s: '36px', d: 'retro' })} alt={user.nickname} />
+      </div>
+      <div className="chat-text">
+        <div className="chat-user">
+          <b>{user.nickname}</b>
+          <span>{data.createdAt}</span>
+          <p>{data.content}</p>
+        </div>
+      </div>
+    </ChatWrapper>
+  );
 };
 
 export default Chat;
