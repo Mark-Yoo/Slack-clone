04817a9a9af5fb3135533773c51863dc14fb52a9
Mark Yoo <yjinhyuk@gmail.com>
2021-05-04 20:55:44 +0900

    Update: components/ChatBox, /ChatList Detail: Chat lists are now shown

--- front/components/ChatBox/index.tsx
+++ front/components/ChatBox/index.tsx
@@ -1,6 +1,12 @@
 import React, { useCallback, useEffect, useRef, VFC } from 'react';
-import { ChatArea, Form, MentionsTextarea, SendButton, Toolbox } from '@components/ChatBox/styles';
 import autosize from 'autosize';
+import useSWR from 'swr';
+import gravatar from 'gravatar';
+import { Mention, SuggestionDataItem } from 'react-mentions';
+import { useParams } from 'react-router';
+import { ChatArea, EachMention, Form, MentionsTextarea, SendButton, Toolbox } from '@components/ChatBox/styles';
+import fetcher from '@utils/fetcher';
+import { IUser } from '@typings/db';
 
 interface Props {
   chat: string;
@@ -10,21 +16,53 @@ interface Props {
 }
 
 const ChatBox: VFC<Props> = ({ chat, onChangeChat, onSubmitForm, placeholder }) => {
+  const { workspace } = useParams<{ workspace: string }>();
+
+  const { data: userData, error, revalidate, mutate } = useSWR<IUser | false>(`/api/users`, fetcher, {
+    dedupingInterval: 2000,
+  });
+  const { data: memberData } = useSWR<IUser[]>(userData ? `/api/workspaces/${workspace}/members` : null, fetcher);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   useEffect(() => {
     if (textareaRef.current) {
       autosize(textareaRef.current);
     }
   }, []);
-  const onKeyDownChat = useCallback((e) => {
-    // console.log(e);
-    if (e.key === 'Enter') {
-      if (!e.shiftKey) {
-        e.preventDefault();
-        onSubmitForm(e);
+  const onKeyDownChat = useCallback(
+    (e) => {
+      // console.log(e);
+      if (e.key === 'Enter') {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          onSubmitForm(e);
+        }
       }
-    }
-  }, []);
+    },
+    [onSubmitForm],
+  );
+
+  const renderSuggestion = useCallback(
+    (
+      suggestion: SuggestionDataItem,
+      search: string,
+      highlightedDisplay: React.ReactNode,
+      index: number,
+      focused: boolean,
+    ): React.ReactNode => {
+      if (!memberData) return;
+      return (
+        <EachMention focus={focused}>
+          <img
+            src={gravatar.url(memberData[index].email, { s: '20px', d: 'retro' })}
+            alt={memberData[index].nickname}
+          />
+          <span>{highlightedDisplay}</span>
+        </EachMention>
+      );
+    },
+    [],
+  );
 
   return (
     <ChatArea>
@@ -34,8 +72,16 @@ const ChatBox: VFC<Props> = ({ chat, onChangeChat, onSubmitForm, placeholder }) 
           onChange={onChangeChat}
           onKeyDown={onKeyDownChat}
           placeholder={placeholder}
-          ref={textareaRef}
-        />
+          inputRef={textareaRef}
+          allowSuggestionsAboveCursor
+        >
+          <Mention
+            appendSpaceOnAdd
+            trigger="@"
+            data={memberData?.map((v) => ({ id: v.id, display: v.nickname })) || []}
+            renderSuggestion={renderSuggestion}
+          />
+        </MentionsTextarea>
         <Toolbox>
           <SendButton
             className={
--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useCallback, useRef, VFC } from 'react';
+import Chat from '@components/Chat';
 import { ChatZone, Section } from '@components/ChatList/styles';
+import { IDM } from '@typings/db';
+import { Scrollbars } from 'react-custom-scrollbars';
 
-const ChatList = () => {
+interface Props {
+  chatData?: IDM[];
+}
+
+const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
+
   return (
     <ChatZone>
-      <Section>Chat Section</Section>
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
