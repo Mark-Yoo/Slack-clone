62abadc402fd39d488738b3e5fed0f8a0eaa5349
Mark Yoo <yjinhyuk@gmail.com>
2021-05-04 21:27:13 +0900

    Update: pages/DirectMessage Detail: user nickname right next to icon is now added

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -49,6 +49,7 @@ const DirectMessage: FC = () => {
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        {userData.nickname}
       </Header>
       <ChatList chatData={chatData} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
