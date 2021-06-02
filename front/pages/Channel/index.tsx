b6eba2914ed337d744d66b83933f3f9b10793a06
Mark Yoo <yjinhyuk@gmail.com>
2021-06-02 19:38:46 +0900

    Update: Channel component is now set

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,21 +1,152 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import ChatList from '@components/ChatList';
 import ChatBox from '@components/ChatBox';
 import useInput from '@hooks/useInput';
 import { Header, Container } from '@pages/Channel/styles';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import useSWR, { useSWRInfinite } from 'swr';
+import fetcher from '@utils/fetcher';
+import { IChannel, IChat, IUser } from '@typings/db';
+import useSocket from '@hooks/useSocket';
+import Scrollbars from 'react-custom-scrollbars';
+import makeSection from '@utils/makesection';
+import InviteChannelModal from '@components/InviteChannelModal';
 
 const Channel: FC = () => {
-  const [chat, onChangeChat] = useInput('');
+  const [chat, onChangeChat, setChat] = useInput('');
 
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const { data: channelData } = useSWR<IChannel>(`/api/workspaces/${workspace}/channels/${channel}`, fetcher);
+  const { data: chatData, mutate: mutateChat, revalidate: revalidateChat, setSize } = useSWRInfinite<IChat[]>(
+    (index: number) => `/api/workspaces/${workspace}/channels/${channel}/chats?perPage=20&page=${index + 1}`,
+    fetcher,
+  );
+
+  const { data: channelMembersData } = useSWR<IUser[]>(
+    myData ? `/api/workspaces/${workspace}/channels/${channel}/members` : null,
+  );
+
+  const [socket] = useSocket(workspace);
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef<Scrollbars>(null);
+  const [showInviteChannelModal, setShowInviteChannelModal] = useState(false);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim() && chatData && channelData) {
+        mutateChat((prevChatData) => {
+          prevChatData?.[0].unshift({
+            id: (chatData[0][0]?.id || 0) + 1,
+            content: chat,
+            UserId: myData.id,
+            User: myData,
+            ChannelId: channelData.id,
+            Channel: channelData,
+            createdAt: new Date(),
+          });
+          return prevChatData;
+        }).then(() => {
+          setChat('');
+          scrollbarRef.current?.scrollToBottom();
+        });
+        axios
+          .post(`/api/workspaces/${workspace}/channels/${channel}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidateChat();
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, chatData, myData, channelData, workspace, channel],
+  );
+
+  const onMessage = useCallback(
+    (data: IChat) => {
+      if (data.Channel.name === channel && data.UserId !== myData?.id) {
+        mutateChat((chatData) => {
+          chatData?.[0].unshift(data);
+          return chatData;
+        }, false).then(() => {
+          if (scrollbarRef.current) {
+            if (
+              scrollbarRef.current.getScrollHeight() <
+              scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
+            ) {
+              console.log('scrollToBottom', scrollbarRef.current?.getValues());
+              setTimeout(() => {
+                scrollbarRef.current?.scrollToBottom();
+              }, 50);
+            }
+          }
+        });
+      }
+    },
+    [channel],
+  );
+
+  useEffect(() => {
+    socket?.on('message', onMessage);
+    return () => {
+      socket?.off('message', onMessage);
+    };
+  }, [socket, onMessage]);
+
+  // 로딩 할 때에 스크롤이 맨 아래를 향하도록 함
+  useEffect(() => {
+    if (chatData?.length === 1) {
+      scrollbarRef.current?.scrollToBottom();
+    }
+  }, [chatData, myData]);
+
+  const onClickInviteChannel = useCallback(() => {
+    setShowInviteChannelModal(true);
   }, []);
+
+  const onCloseModal = useCallback(() => {
+    setShowInviteChannelModal(false);
+  }, []);
+
+  if (!myData) {
+    return null;
+  }
+
+  const chatSections = makeSection(chatData ? chatData?.flat().reverse() : []);
 
   return (
     <Container>
-      <Header>Channel</Header>
-      {/* <ChatList /> */}
+      <Header>
+        <span>#{channel}</span>
+        <div className="header-right">
+          <span>{channelMembersData?.length}</span>
+          <button
+            onClick={onClickInviteChannel}
+            className="c-button-unstyled p-ia__view_header__button"
+            aria-label="Add people to #channel"
+            data-sk="tooltip_parent"
+            type="button"
+          >
+            <i className="c-icon p-ia__view_header__button_icon c-icon--add-user" aria-hidden="true" />
+          </button>
+        </div>
+      </Header>
+      <ChatList
+        chatSections={chatSections}
+        ref={scrollbarRef}
+        setSize={setSize}
+        isEmpty={isEmpty}
+        isReachingEnd={isReachingEnd}
+      />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+      <InviteChannelModal
+        show={showInviteChannelModal}
+        onCloseModal={onCloseModal}
+        setShowInviteChannelModal={setShowInviteChannelModal}
+      />
     </Container>
   );
 };
