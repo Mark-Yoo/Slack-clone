53d1ce74b72ca4854f4e98ec6f986080b9f4775c
Mark Yoo <yjinhyuk@gmail.com>
2021-06-07 16:16:05 +0900

    Update: webpack-bundle-analyzer is now set. Follwing mode will change the way how it displays

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -93,7 +93,7 @@ if (isDevelopment && config.plugins) {
   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
 }
 if (!isDevelopment && config.plugins) {
-  // config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
 }
 
